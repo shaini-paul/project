b2ab2427f75484f58ccfa168635157b1439aa158
shaini-paul <39427494+shaini-paul@users.noreply.github.com>
2018-08-18 16:37:20 +0530

    Create mail.php

---
+++ mail.php
@@ -0,0 +1,41 @@
+Its been 2 weeks since I re-started working with you.
+
+I have given you some WP Plugin related  work that you was working from last 15-20 days 
+with Manish Sir for a Plugin enhancement feature.
+
+I have discuses with you for the features and the task you needs to do, and have done possible support.
+
+First few days I found that you were struggling to understand the code, 
+and only added few code for adding one feature but that also not 
+fully completed.
+
+While working with you I also found that You also having problem in understanding 
+what code is actually doing, and only have guess that code is doing something.
+
+The major Problem I found that you are lacking :
+
+- Failed to do basic If/else logic in code, and struggling what do be done.
+
+- You failed to decide what feature I should use and how that will 
+be useful.
+
+- You need longer time to complete that task.
+
+- Lacking in testing for various possible cases.
+
+The problem is not with WordPress code, but the problem is CORE PHP/MYSQL logics. 
+So I found that you have less capability to do some logic programmings.
+
+
+In past when I given few projects to you, that were also delayed, but so 
+far you had enough time to learn her week points, and should have improved.
+
+As far as I remember We have given her basic trainings for HTML/CSS too.
+
+
+So you need a through understand of work/task, and need them to be completed on time.
+I can see that you now learning things that should have done quicker.
+
+
+Please do a focus on this note. 
+
